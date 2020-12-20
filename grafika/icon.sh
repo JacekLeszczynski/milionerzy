fac45b5942589badfe3c0300599af9aa9af7ebf4
Jacek Leszczyński <sam@bialan.pl>
2020-12-20 13:06:08 +0100

    Zmiana ikony

---
+++ grafika/icon.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+NAME="milionerzy"
+
+#convert ${NAME}.png -resize 48x48 ${NAME}_48x48.png
+#convert ${NAME}.png -resize 36x36 ${NAME}_36x36.png
+#convert ${NAME}.png -resize 24x24 ${NAME}_24x24.png
+
+convert ${NAME}.png -resize 128x128 ${NAME}_128x128.png
